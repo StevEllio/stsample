453898088364b6170a289debbad77ece39716c56
StevEllio <122404730+StevEllio@users.noreply.github.com>
2024-11-19 12:32:57 +0000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+# Import python packages
+import streamlit as st
+import requests
+import pandas as pd
+
+
+# Write directly to the app
+st.title("Navigation testing")
+st.write(
+    """sub heading
+    """
+)
