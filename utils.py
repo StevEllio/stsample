ca024b0220e6c9878c4e7b2aa3a1969ed5d3e5fc
StevEllio <122404730+StevEllio@users.noreply.github.com>
2024-12-01 18:44:31 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,9 +3,13 @@ import datetime
 
 class DatesFromPeriod(enum):
     MTD = datetime.date.today().replace(day=1), datetime.date.today()
+    YTD = datetime.date.today().replace(day=1,month=1), datetime.date.today()
     
     def __init__(self,start_date,end_date)
         self.StartDate = start_date
         self.EndDate = end_date
-        
+    
+    @staticmethod
+    def list():
+        return list(map(datetime.date,DatesFromPeriod)
         
