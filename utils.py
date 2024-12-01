643e8ed6a6a1f8d7575f198b7f72cb6d496500f1
StevEllio <122404730+StevEllio@users.noreply.github.com>
2024-12-01 12:03:13 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from enum import enum
 import datetime
 
 class DatesFromPeriod(enum):
-    MTD = datetime.date.today(), datetime.date.today()
+    MTD = datetime.date.today().replace(day=1), datetime.date.today()
     
     def __init__(self,start_date,end_date)
         self.StartDate = start_date
